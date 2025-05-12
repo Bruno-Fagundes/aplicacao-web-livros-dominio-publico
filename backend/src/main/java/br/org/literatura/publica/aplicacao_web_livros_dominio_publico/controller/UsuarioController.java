e863bef9cb005b24cb63a8e756845c4b922d1fac
Bruno Fagundes <brunogarciafagundes@gmail.com>
2025-05-12 20:47:41 -0300

    update: mudança no arquivos

--- backend/src/main/java/br/org/literatura/publica/aplicacao_web_livros_dominio_publico/controller/UsuarioController.java
+++ backend/src/main/java/br/org/literatura/publica/aplicacao_web_livros_dominio_publico/controller/UsuarioController.java
@@ -1,4 +1,25 @@
 package br.org.literatura.publica.aplicacao_web_livros_dominio_publico.controller;
 
+import br.org.literatura.publica.aplicacao_web_livros_dominio_publico.dto.UsuarioDto;
+import br.org.literatura.publica.aplicacao_web_livros_dominio_publico.model.Usuario;
+import br.org.literatura.publica.aplicacao_web_livros_dominio_publico.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public List<UsuarioDto> listar() {
+        List<Usuario> usuarios = usuarioRepository.findAll();
+
+    }
 }
--- backend/src/main/java/br/org/literatura/publica/aplicacao_web_livros_dominio_publico/dto/UsuarioDto.java
+++ backend/src/main/java/br/org/literatura/publica/aplicacao_web_livros_dominio_publico/dto/UsuarioDto.java
@@ -1,4 +1,33 @@
 package br.org.literatura.publica.aplicacao_web_livros_dominio_publico.dto;
 
+import br.org.literatura.publica.aplicacao_web_livros_dominio_publico.model.StatusUsuario;
+import br.org.literatura.publica.aplicacao_web_livros_dominio_publico.model.Usuario;
+
+import java.time.LocalDateTime;
+
 public class UsuarioDto {
+
+    private Long id;
+    private String nomeUsuario;
+    private String email;
+    private String senha;
+    private StatusUsuario status;
+    private String funcao;
+    private String fotoPerfilUrl;
+    private LocalDateTime atualizadoEm;
+    private LocalDateTime criadoEm;
+    private LocalDateTime ultimoAcesso;
+
+    public UsuarioDto (Usuario usuario) {
+        super();
+        this.nomeUsuario = usuario.getNomeUsuario();
+        this.email = usuario.getEmail();
+        this.senha = usuario.getSenha();
+        this.status = usuario.getStatus();
+        this.funcao = "USER";
+        this.fotoPerfilUrl = "frontend/src/assets/images/foto-perfil-usuario/foto-perfil.png";
+        this.atualizadoEm = usuario.getAtualizadoEm();
+        this.criadoEm = usuario.getCriadoEm();
+        this.ultimoAcesso = usuario.getUltimoAcesso();
+    }
 }
