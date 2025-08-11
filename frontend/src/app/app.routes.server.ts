057363b9b1b1ed5600fd9a9e61a519ec063facf0
Bruno Fagundes <brunogarciafagundes@gmail.com>
2025-08-11 14:25:36 -0300

    tela livros detalhes

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,18 +1,21 @@
 // src/app/app.routes.server.ts
-import { RenderMode, PrerenderFallback, ServerRoute } from '@angular/ssr';
+import { RenderMode, ServerRoute } from '@angular/ssr';
 import { inject } from '@angular/core';
-import { LivroService } from './services/livro.service'; // ajusta o caminho
+import { LivroService } from './services/livro.service';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: '', renderMode: RenderMode.Prerender },                 // prerender /
+  { path: 'pagina-inicial', renderMode: RenderMode.Prerender },  // etc.
   {
     path: 'livros/:id',
     renderMode: RenderMode.Prerender,
-    fallback: PrerenderFallback.Client, // opcional: se um id não estiver pré-renderizado, cai para CSR
     async getPrerenderParams() {
-      // Nota: inject deve ser chamado sincronamente
-      const livroService = inject(LivroService);
-      const ids = await livroService.listarIdsDeLivros() ?? []; // garante que ids seja um array
-      return ids.map((id: any) => ({ id: String(id) })); // formato: [{id: '1'}, {id: '2'}]
+      const srv = inject(LivroService);
+      // Aqui tipamos explicitamente o retorno
+      const ids = srv.listarIdsDeLivros() as unknown as string[];
+      // Tipamos 'id' no map
+      return ids.map((id: string) => ({ id }));
     }
-  }
+  },
+  { path: '**', renderMode: RenderMode.Server }
 ];
---
+++ frontend/src/app/components/autor-detalhes/autor-detalhes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autor-detalhes',
+  imports: [],
+  templateUrl: './autor-detalhes.component.html',
+  styleUrl: './autor-detalhes.component.scss'
+})
+export class AutorDetalhesComponent {
+
+}
