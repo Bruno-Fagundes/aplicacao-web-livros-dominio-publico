eebe78cb4d3dcce6161a3f5c6e2eff7681454d59
Bruno Fagundes <134444770+Bruno-Fagundes@users.noreply.github.com>
2026-01-28 14:02:21 -0300

    Update AutorController.java

--- backend/src/main/java/br/org/literatura/publica/aplicacao_web_livros_dominio_publico/controller/AutorController.java
+++ backend/src/main/java/br/org/literatura/publica/aplicacao_web_livros_dominio_publico/controller/AutorController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/autores")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://literaturapublica.vercel.app/")
 @RequiredArgsConstructor
 public class AutorController {
 
