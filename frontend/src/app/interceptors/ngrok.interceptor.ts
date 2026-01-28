2f9ffd9a53d41c39026402e872e7e2f688c13b82
Bruno Fagundes <134444770+Bruno-Fagundes@users.noreply.github.com>
2026-01-28 16:09:38 -0300

    Update ngrok.interceptor.ts

--- frontend/src/app/interceptors/ngrok.interceptor.ts
+++ frontend/src/app/interceptors/ngrok.interceptor.ts
@@ -1,9 +1,15 @@
-// ngrok.interceptor.ts
-intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-  const ngrokReq = req.clone({
-    setHeaders: {
-      'ngrok-skip-browser-warning': 'true'
-    }
-  });
-  return next.handle(ngrokReq);
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class NgrokInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const ngrokReq = req.clone({
+      setHeaders: {
+        'ngrok-skip-browser-warning': 'true'
+      }
+    });
+    return next.handle(ngrokReq);
+  }
 }
