04332707904b0ff669cd5fcf05d8566f2af84e9d
Bruno Fagundes <134444770+Bruno-Fagundes@users.noreply.github.com>
2026-01-30 12:07:19 -0300

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://unvolitional-insensately-mazie.ngrok-free.dev'
+  apiUrl: 'https://unvolitional-insensately-mazie.ngrok-free.dev/api'
 };
